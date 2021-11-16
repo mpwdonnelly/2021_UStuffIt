60ea641404ab8c2949d73e2cfced1d09d2aabbc5
asnyder8 <63461278+asnyder8@users.noreply.github.com>
2021-11-15 21:36:33 -0600

    testing manual connection creds

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -4,10 +4,10 @@ module.exports = {
     PASSWORD: "97b196e101b6476558433d4b5333e28e214377f80793107c8f12d6370a3d66dd",
     DB: "d8j1s1gfdhleha",
     dialect: "postgres",
-    // pool: {
-    //     max: 5,
-    //     min: 0,
-    //     acquire: 30000,
-    //     idle: 10000
-    // }
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
 };
