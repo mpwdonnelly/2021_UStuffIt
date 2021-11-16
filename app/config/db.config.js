7369f942493adf20c02ecdced1d3f69bb4cd5730
asnyder8 <63461278+asnyder8@users.noreply.github.com>
2021-11-15 23:11:16 -0600

    more testing for ECONN err

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,19 +1,19 @@
-module.exports = {
-    HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "mysql",
-    DB: "catalogdb",
-    dialect: "postgres",
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    },
-    pool: {
-        max: 20,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-};
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "postgres",
+//     PASSWORD: "mysql",
+//     DB: "catalogdb",
+//     dialect: "postgres",
+//     dialectOptions: {
+//         ssl: {
+//             require: true,
+//             rejectUnauthorized: false
+//         }
+//     },
+//     pool: {
+//         max: 20,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
--- app/models/index.js
+++ app/models/index.js
@@ -1,16 +1,27 @@
 const dbConfig = require("../config/db.config");
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-    host: dbConfig.HOST,
-    dialect: dbConfig.dialect,
-    operatorsAliases: false,
+// const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+//     host: dbConfig.HOST,
+//     dialect: dbConfig.dialect,
+//     operatorsAliases: false,
 
-    pool: {
-        max: dbConfig.pool.max,
-        min: dbConfig.pool.min,
-        acquire: dbConfig.pool.acquire,
-        idle: dbConfig.pool.idle
+//     pool: {
+//         max: dbConfig.pool.max,
+//         min: dbConfig.pool.min,
+//         acquire: dbConfig.pool.acquire,
+//         idle: dbConfig.pool.idle
+//     }
+// });
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
     }
 });
 
