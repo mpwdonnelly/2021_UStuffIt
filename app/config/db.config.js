df71fc7fd3d264e2c3788d340ed5aaa53d4ab85d
asnyder8 <63461278+asnyder8@users.noreply.github.com>
2021-11-15 21:33:23 -0600

    testing further

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "mysql",
-    DB: "catalogdb",
+    HOST: "ec2-52-72-155-37.compute-1.amazonaws.com",
+    USER: "jkjadfgtuwrxtr",
+    PASSWORD: "97b196e101b6476558433d4b5333e28e214377f80793107c8f12d6370a3d66dd",
+    DB: "d8j1s1gfdhleha",
     dialect: "postgres",
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
+    // pool: {
+    //     max: 5,
+    //     min: 0,
+    //     acquire: 30000,
+    //     idle: 10000
+    // }
 };
