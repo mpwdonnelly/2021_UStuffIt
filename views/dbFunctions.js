010d1dfa4530a800a2981f23cfd3b5f6d70907c9
asnyder8 <63461278+asnyder8@users.noreply.github.com>
2021-11-16 01:01:31 -0600

    added form clear funcationality after creating item

--- views/dbFunctions.js
+++ views/dbFunctions.js
@@ -26,7 +26,21 @@ function doPost(){
     }`;
 
     xhr.send(data);
+
+    //clear form
+    document.getElementById("thing_label").value = "";
+    document.getElementById("thing_status").value = "";
+    document.getElementById("thing_condition").value = "";
+    document.getElementById("person_role").value = "";
+    document.getElementById("person_contactInfo").value = "";
+    document.getElementById("place_storedIn").value = "";
+    document.getElementById("category_label").value = "";
+    document.getElementById("hist_desc").value = "";
+    document.getElementById("hist_date").value = "";
+    document.getElementById("artifact_type").value = "";
+    document.getElementById("imgLink").value = "";
 };
+
 function doGet(){
     var url = "/api/catalogs";
     var xhr = new XMLHttpRequest();
@@ -39,8 +53,6 @@ function doGet(){
         console.log(xhr.status);
         console.log(xhr.responseText);
     }};
-
-    var data = `{}`;
 
     xhr.send(data);
 };
@@ -57,8 +69,6 @@ function deleteAll() {
         console.log(xhr.status);
         console.log(xhr.responseText);
     }};
-
-    //var data = `{}`;
 
     xhr.send();
 }
