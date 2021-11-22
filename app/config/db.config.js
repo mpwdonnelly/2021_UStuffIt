4f4d78ec8588c50b88032207c9076019f35ecf97
asnyder8 <63461278+asnyder8@users.noreply.github.com>
2021-11-21 20:52:10 -0600

    single routing search by ID added, form tweaked, DEV mode for local work enabled. Heroku live disabled for now.

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -3,22 +3,22 @@
 // DO NOT RE-ENABLE, SAVING FOR LEGACY. ONLY RE-ENABLE FOR LOCAL
 // NODE SERVER.JS WORK -AS 11/15/21
 //***************************************************************/
-// module.exports = {
-//     HOST: "localhost",
-//     USER: "postgres",
-//     PASSWORD: "mysql",
-//     DB: "catalogdb",
-//     dialect: "postgres",
-//     dialectOptions: {
-//         ssl: {
-//             require: true,
-//             rejectUnauthorized: false
-//         }
-//     },
-//     pool: {
-//         max: 20,
-//         min: 0,
-//         acquire: 30000,
-//         idle: 10000
-//     }
-// };
+module.exports = {
+    HOST: "localhost",
+    USER: "postgres",
+    PASSWORD: "mysql",
+    DB: "catalogdb",
+    dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
+    pool: {
+        max: 20,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
