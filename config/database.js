6390f5ac1e7ff0c2be02d6d8102b0003e6fb83da
asnyder8 <63461278+asnyder8@users.noreply.github.com>
2021-11-30 12:30:36 -0600

    testing heroku deploy

--- config/database.js
+++ config/database.js
@@ -2,16 +2,27 @@ const Sequelize = require('sequelize');
 
 // for the localhost postgres db on the backend
 // change these credentials to fit your own setup
-module.exports = new Sequelize('catalogdb', 'miked', 'D92t84ow!', {
-  host: 'localhost',
-  dialect: 'postgres',
-  operatorAliases: false,
+// module.exports = new Sequelize('catalogdb', 'miked', 'D92t84ow!', {
+//   host: 'localhost',
+//   dialect: 'postgres',
+//   operatorAliases: false,
 
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   },
+// });
+
+module.exports = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+      ssl: {
+          require: true,
+          rejectUnauthorized: false
+      }
+  }
 });
 
